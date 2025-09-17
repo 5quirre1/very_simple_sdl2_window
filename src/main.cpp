dbbfa9983de004dd30d9ce8e83634653b1f101f3
Squirrel <squirrelhomebrew@gmail.com>
2025-09-16 20:55:16 -0700

    yes

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,56 @@
 #define SDL_MAIN_HANDLED
 #include "SDL2/SDL.h"
-#include "hexrgbSDL.h"
+#include "game.hpp"
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(void)
 {
-  // init sdl
-  if (SDL_Init(SDL_INIT_VIDEO) != 0)
-  {
-    cerr << "sdl_init error: " << SDL_GetError() << "\n";
-    return 1;
-  }
-
-  // create window
-  const int screen_width = 500;
-  const int screen_height = 500;
-
-  SDL_Window *window = SDL_CreateWindow(
-      "greg",
-      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-      screen_width, screen_height,
-      SDL_WINDOW_SHOWN
-  );
-  
-  // error handle for if window no create
-  if (!window)
-  {
-    cerr << "window didn't get created, error: " << SDL_GetError() << "\n";
-    SDL_Quit();
-    return 1;
-  }
-
-  // renderer
-  SDL_Renderer* renderer = SDL_CreateRenderer(
-    window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-  );
-
-  // renderer error
-  if (!renderer)
-  {
-    cerr << "renderer was not created; error: " << SDL_GetError() << "\n";
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 1;
-  }
-
-  bool quit = false;
-  SDL_Event e;
-
-  // main loop
-  while (!quit)
-  {
-    // events
-    while (SDL_PollEvent(&e) != 0)
+    // init sdl
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
-      // exit
-      if (e.type == SDL_QUIT)
-      {
-        quit = true;
-      }
+        cerr << "sdl_init error: " << SDL_GetError() << "\n";
+        return 1;
     }
 
-    // clear screen and make with prettu color
-    SDL_Color peak = fixColors("#0f3f67ff");
-    SDL_SetRenderDrawColor(renderer, peak.r, peak.g, peak.b, peak.a);
-    SDL_RenderClear(renderer);
+    const int screen_width = 500;
+    const int screen_height = 500;
 
-    
-    SDL_RenderPresent(renderer);
-  }
-  
-  // cleanup the stuff
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    // create window
+    SDL_Window *window = SDL_CreateWindow(
+        "greg",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        screen_width, screen_height,
+        SDL_WINDOW_SHOWN
+    );
 
-  return 0;
+    if (!window)
+    {
+        cerr << "window didn't get created, error: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    // create renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    if (!renderer)
+    {
+        cerr << "renderer was not created; error: " << SDL_GetError() << "\n";
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // run the game loop
+    runGame(window, renderer);
+
+    // cleanup
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
